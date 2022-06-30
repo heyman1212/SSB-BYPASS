96b5596a6561cf3833b18fa99d13eed57c83f89c
heyman1212 <107253355+heyman1212@users.noreply.github.com>
2022-06-30 19:38:02 +0600

    Add files via upload

---
+++ FUCKED-SSB.py
@@ -0,0 +1,24 @@
+import os, platform, time
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("      \x1b[1;91m\x1b[1;47m Totaly Cradit to \x1b[0m\x1b[1;97m\x1b[1;41m Zaid Rao ;3 \x1b[0m")
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("\n\x1b[1;92m         Toll Loading Please Wait... ") 
+
+    os.system('xdg-open https://youtube.com/channel/UC6OSDzf3I3Ws8onwga637uA/');time.sleep(7)
+
+    import SSB
+
+elif b == '32bit':
+
+    print("\x1b[1;91mOpps Sorry Beb Your Mobile Not Support This Tool")
+
+    os.system('xdg-open xdg-open https://bit.ly/39Q0hn7/');time.sleep(5)
+
+
+
